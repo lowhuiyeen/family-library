9db21aa7a89a706361292ab9ba5c1da1dc3837db
lowhuiyeen <lowhuiyeen@gmail.com>
2025-10-17 18:30:49 +0800

    Update family library

---
+++ assets/firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+// This file stores your Firebase credentials.
+// ⚠️ Do NOT commit this file — it’s added to .gitignore.
+export const firebaseConfig = {
+  apiKey: "AIzaSyBsmHmQoA1sbjAHqUkEWW5MFUQP1_W8Xeg",
+  authDomain: "family-library-76f73.firebaseapp.com",
+  databaseURL: "https://family-library-76f73-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "family-library-76f73",
+  storageBucket: "family-library-76f73.firebasestorage.app",
+  messagingSenderId: "157329873139",
+  appId: "1:157329873139:web:c433c06e146feff8743f86"
+};
